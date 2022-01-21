cdb90a2c66b9daf969e3797a682d4a0579173f46
amanividanage <90959983+amanividanage@users.noreply.github.com>
2022-01-21 20:16:24 +0530

    Add files via upload

---
+++ index11.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Form
+        </title>
+    </head>
+    <body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <label>Select Image File:</label>
+    <input type="file" name="image">
+    <input type="submit" name="submit" value="Upload">
+</form>
+
+    </body>
+</html>
